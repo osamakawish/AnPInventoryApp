541ac8794936b49c63089953e3c74c2dc7baaa3b
Osama Kawish <osamakawish@gmail.com>
2024-10-04 17:30:56 -0400

    Migrating to Avalonia.

---
+++ AnPInventoryAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace AnPInventoryAvalonia.Views;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
